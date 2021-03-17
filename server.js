af44643e64bbc26c6136edcacaafdb78036362c2
George Kavallieratos <georgealkaval@gmail.com>
2021-03-17 10:54:23 -0400

    set us server.js file

---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+const session = require('express-session')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+const mongoURI = process.env.MONGODBURI
+const db = mongoose.connection
+mongoose.connect(mongoURI, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, () => {
+  console.log("database connection checked")
+})
+db.on('error', (err) => {
+  console.log('ERROR: ', err)
+})
+db.on('connected', () => {
+  console.log('mongo connected')
+})
+db.on('disconnected', () => {
+  console.log('mongo disconnected')
+})
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret:process.env.SECRET,
+  resave: false,
+  saveUninitiaized: false,
+}))
+const ufcControllers = require('./controllers/ufc')
+const usersControllers = require('./controllers/users')
+const sessionsControllers = require('./controllers/sessions')
+const bellatorControllers = require('./controllers/bellator')
+const onefcControllers = require('./controllers/onefc')
+// app.use('/ufc', ufcControllers)
+// app.use('/users', usersControllers)
+// app.use('/sessions', sessionsControllers)
+// app.use('/bellator', bellatorControllers)
+// app.use('/onefc', onefcControllers)
+
+// app.get('/', (req, res) => {
+//   res.render('home.ejs', {
+//     currentUser: req.session.currentUser
+//   })
+// })
+app.listen(PORT, () => {
+  console.log('Server is listening!!!');
+})
