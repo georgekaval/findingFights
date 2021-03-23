b90f6d63f7f493e8da26708cc3019e05613a90ba
George Kavallieratos <georgealkaval@gmail.com>
2021-03-23 08:54:09 -0400

    added fight button style

--- todo.js
+++ todo.js
@@ -1,1 +1,5 @@
-//Do i need to make a new model for the other mma organizations or fit them
+//Make footer sticky?
+//Make footer text center
+//Index & show pages need images or something to fill in the empty space
+//What will be the value of signing in for regular users?  Right now it doesnt serve a purpose.  Do I make the fights not visible for non users?  Is it just for potential forums etc?
+//
