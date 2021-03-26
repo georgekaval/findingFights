a60590a6f343f82d43b464c5b46e9c69b89919ba
George Kavallieratos <georgealkaval@gmail.com>
2021-03-26 15:38:17 -0400

    minor changes

--- todo.js
+++ todo.js
@@ -4,3 +4,17 @@
 //Index & show pages need images or something to fill in the empty space
 //What will be the value of signing in for regular users?  Right now it doesnt serve a purpose.  Do I make the fights not visible for non users?  Is it just for potential forums etc?
 //Style on index page, the main event part
+//          <% let countdown = ''%>
+          // <%const countDownDate = fights[i].date.getTime()%>
+          // <% const callCountdown = setInterval(() => { %>
+          //   <%  const now = new Date().getTime()%>
+          //   <% const distance = countDownDate - now %>
+          //   <% const days = Math.floor(distance / (1000 * 60 * 60 * 24)) %>
+          //   <% const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))%>
+          //   <% const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))%>
+          //   <% const seconds = Math.floor((distance % (1000 * 60)) / 1000)%>
+          //   <% let countdown = days + "d " + hours + "h "
+          //   + minutes + "m " + seconds + "s "%>
+          //   <%console.log(countdown)%>
+          // <% }, 1000) %>
+          // <h4><%=countdown%></h4>
